3f9c0b326a54a30a5054ff595bc69f6ce965a439
Prime <singhamish27@gmail.com>
2020-06-13 20:01:08 +0530

    applied css to login and register

--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -4,6 +4,7 @@ import { Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {login, loadUser} from '../../actions/auth';
 import Styles from './login.module.css';
+import { $CombinedState } from 'redux';
 
 const Login = ({login, isAuthenticated, loadUser}) => {
 
@@ -28,20 +29,23 @@ const Login = ({login, isAuthenticated, loadUser}) => {
     }
 
     return(
-        <div className={Styles.page}>
-                <h1 className={Styles.heading}>Login to your account</h1>
+        <section className={Styles.loginpage}>
+            <div className={Styles.loginform}>
+                <h1 className={Styles.heading}>Login to Your Account</h1>
                 <form className='form' onSubmit={e => onSubmit(e)}>
                     <div className="form-group">
                         <input type="number" className="form-control" placeholder="Enter phone"  name="phone" value={phone} onChange={e => onChange(e)} size='10' required/>
-                        <small className='form-text'>Enter number without +91</small>
+                        <small className='form-text' className={Styles.phoneinfo}>Number should be without +91</small>
                     </div>
                     <div className="form-group">
                         <input type="password" className="form-control" placeholder="Enter password"  name="password" value={password} onChange={e => onChange(e)} required/>
                     </div>
                     <input type="submit" className="btn btn-primary" value="Login" />
                 </form>
-        </div>
+            </div>
+        </section>  
     )
+
 };
 
 Login.prototype = {
@@ -53,5 +57,7 @@ Login.prototype = {
 const mapStateToProps = state =>({
     isAuthenticated: state.auth.isAuthenticated
 });
+
+
 
 export default connect(mapStateToProps, {login, loadUser})(Login);
--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -4,6 +4,7 @@ import { setAlert } from '../../actions/alert';
 import { register, loadUser } from '../../actions/auth';
 import PropTypes from 'prop-types'
 import { Redirect } from 'react-router-dom';
+import Styles from './register.module.css';
 
 
 const Register = (props) => {
@@ -34,28 +35,28 @@ const Register = (props) => {
     }
 
     return(
-        <Fragment>
-                <h1>Create your account</h1>
+        <section className={Styles.registerpage}>
+            <div className={Styles.registerform}>
+                <h1 className={Styles.heading}>Create Your Account</h1>
                 <form className='form' onSubmit={e => onSubmit(e)}>
                     <div className="form-group">
                         <input className="form-control" placeholder="Enter name" name="name" value={name} onChange={e => onChange(e)} required/>
                     </div>
                     <div className="form-group">
                         <input type="number" className="form-control" placeholder="Enter phone"  name="phone" value={phone} onChange={e => onChange(e)} size='10' required/>
-                        <small className='form-text'>Enter number without +91</small>
+                        <small className='form-text' className={Styles.info}>Enter number without +91</small>
                     </div>
                     <div className="form-group">
                         <input type="password" className="form-control" placeholder="Enter password"  name="password" value={password} onChange={e => onChange(e)} required/>
-                        <small className='form-text'>Enter password of length 6 and above</small>
+                        <small className='form-text' className={Styles.info}>Enter password of length 6 and above</small>
                     </div>
                     <div className="form-group">
                         <input type="password" className="form-control" placeholder="Enter password again"  name="password2" value={password2} onChange={e => onChange(e)} required/>
                     </div>
                     <input type="submit" className="btn btn-primary" value="Register" />
                 </form>
-                
-                
-        </Fragment>
+            </div>
+        </section>
     )
 };
 
