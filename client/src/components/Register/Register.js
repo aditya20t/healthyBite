363859dd5da3c83cca5670166180db28e7ff29cd
Prime <singhamish27@gmail.com>
2020-06-16 11:35:05 +0530

    added css to register page

--- client/src/components/Register/Register.js
+++ client/src/components/Register/Register.js
@@ -18,46 +18,59 @@ const Register = (props) => {
 
     const { name, phone, password, password2 } = formData;
 
-    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+    const onChange = e => setFormData({ ...formData, [e.target.name]: e.target.value });
 
     const onSubmit = async e => {
         e.preventDefault();
-        if(password !== password2) {
+        if (password !== password2) {
             props.setAlert('Password not match', 'danger');
         } else {
-            props.register({name, phone, password});
+            props.register({ name, phone, password });
         }
     }
 
-    if(props.isAuthenticated) {
+    if (props.isAuthenticated) {
         props.loadUser();
         return <Redirect to='/' />
     }
 
-    return(
-        <section className={Styles.registerpage}>
-            <div className={Styles.registerform}>
-                <h1 className={Styles.heading}>Create Your Account</h1>
-                <form className='form' onSubmit={e => onSubmit(e)}>
-                    <div className="form-group">
-                        <input className="form-control" placeholder="Enter name" name="name" value={name} onChange={e => onChange(e)} required/>
-                    </div>
-                    <div className="form-group">
-                        <input type="number" className="form-control" placeholder="Enter phone"  name="phone" value={phone} onChange={e => onChange(e)} size='10' required/>
-                        <small className='form-text' className={Styles.info}>Enter number without +91</small>
-                    </div>
-                    <div className="form-group">
-                        <input type="password" className="form-control" placeholder="Enter password"  name="password" value={password} onChange={e => onChange(e)} required/>
-                        <small className='form-text' className={Styles.info}>Enter password of length 6 and above</small>
-                    </div>
-                    <div className="form-group">
-                        <input type="password" className="form-control" placeholder="Enter password again"  name="password2" value={password2} onChange={e => onChange(e)} required/>
-                    </div>
-                    <input type="submit" className="btn btn-primary" value="Register" />
-                </form>
+    return (
+        <section className={Styles.background}>
+            <div className={Styles.registerbox}>
+                <div className={Styles.registerform}>
+                    <h1 className={Styles.heading}>Create Account</h1>
+                    <form className='form' onSubmit={e => onSubmit(e)}>
+                        <div className="form-group">
+                            <input className={Styles.input} placeholder="Enter Your Name" name="name" value={name} onChange={e => onChange(e)} required autoComplete="off" />
+                        </div>
+                        <div className="form-group">
+                            <input className={Styles.input} placeholder="Enter Mobile Number" name="phone" onFocus={numberDisplay} value={phone} onChange={e => onChange(e)} size='10' required autoComplete="off" />
+                            <small className='form-text' id='phone' className={Styles.info}></small>
+                        </div>
+                        <div className="form-group">
+                            <input type="password" className={Styles.input} placeholder="Enter password" onFocus={passDisplay} name="password" value={password} onChange={e => onChange(e)} required />
+                            <small className='form-text' className={Styles.info} id="pass" ></small>
+                        </div>
+                        <div className="form-group">
+                            <input type="password" className={Styles.input} placeholder="Enter password again" name="password2" value={password2} onChange={e => onChange(e)} required />
+                        </div>
+                        <button type="submit" className="btn btn-outline-success btn-lg" id={Styles.register}>Register</button>
+                    </form>
+                </div>
             </div>
         </section>
     )
+
+    function numberDisplay() {
+        let txt = document.getElementById('phone');
+        txt.innerHTML = "Number Should be without +91";
+    }
+
+    function passDisplay() {
+        let txt = document.getElementById('pass');
+        txt.innerHTML = "Password should be of atleast length 6";
+    }
+
 };
 
 Register.propTypes = {
@@ -71,4 +84,4 @@ const mapStateToProps = state => ({
     isAuthenticated: state.auth.isAuthenticated
 });
 
-export default connect(mapStateToProps, { setAlert, register,loadUser })(Register);
+export default connect(mapStateToProps, { setAlert, register, loadUser })(Register);
